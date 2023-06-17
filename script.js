8bc4bbb3e9788ef9a8c235960951a38cf0b1c4b6
Roy Joseph <royjoseph.mec@gmail.com>
2023-06-17 13:59:19 +0530

    Implemented the card addition into the html using DOM manipulation

---
+++ script.js
@@ -0,0 +1,37 @@
+let myLibrary = []
+const libraryContainer = document.getElementById("main-section")
+
+function Book(title,author,pages,read){
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
+}
+
+const one = new Book("Harry potter","J K Rowling",100,false)
+const two = new Book("Flashpoint","DC",400,true)
+
+function addBookToLibrary(){
+    myLibrary.push(one)
+    myLibrary.push(two)
+}
+
+addBookToLibrary()
+
+function displayBook(){
+    myLibrary.forEach(book => {
+        const card = `<div class="card">
+                            <h2>${book.title}</h2>
+                            <h3>${book.author}</h3>
+                            <h4>${book.pages}</h4>
+                        </div>`;
+        const div = document.createElement('div')
+        div.innerHTML = card
+        libraryContainer.appendChild(div)
+    })
+}
+
+displayBook()
+
+
+
